9d53734c63ac27f2c35f1131b42b94e738e78a82
hungvipqt15 <91128905+hungvipqt15@users.noreply.github.com>
2024-08-28 14:55:25 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,88 +1,24 @@
 import streamlit as st
-# from openai import OpenAI
-# from prompt import translate_prompt, transcribe_prompt
-import gradio as gr
-import os
-import sys
-import subprocess
 import whisper
-model = whisper.load_model("medium")
 
-def video2mp3(video_file, output_ext="mp3"):
-    filename, ext = os.path.splitext(video_file)
-    subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{filename}.{output_ext}"],
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.STDOUT)
-    return f"{filename}.{output_ext}"
+st.title("Whisper App")
 
+# upload audio file with streamlit
+audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
 
-
-st.title("🦜🔗 Generate subtitle for video")
-
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+model = whisper.load_model("base")
+st.text("Whisper Model Loaded")
 
 
-with st.form("my_form"):
-    # input_video = '/content/videoplayback2.mp4' ################ chỗ này cần input
-    input_video = st.file_uploader("Choose a video...", type=["mp4"])
-    option_font = st.selectbox(
-        "which word font you want to use?",
-        ("Arial", "Elephant", "Ravie","Times New Roman"),
-    )
-
-    option_size = st.selectbox(
-        "which size you want to use?",
-        ("15", "16", "17","18","19",20),
-    )
+if st.sidebar.button("Transcribe Audio"):
+    if audio_file is not None:
+        st.sidebar.success("Transcribing Audio")
+        transcription = model.transcribe(audio_file.name)
+        st.sidebar.success("Transcription Complete")
+        st.markdown(transcription["text"])
+    else:
+        st.sidebar.error("Please upload an audio file")
 
 
-
-
-
-
-    # temperature = st.text_input("",
-    # placeholder="Temperature"
-    # )
-    # print(type(temperature))
-    # media_type = st.text_input("",
-    #     placeholder="Enter a social media type",
-    # )
-    # number_words = st.text_input("",
-    #     placeholder="Enter number of words you want to generate",
-    # )
-    # input_content = st.text_input("",
-    #     placeholder="Enter your input content",
-    # )
-    # style_content = st.text_input("",
-    #     placeholder="Enter your style content",
-    # )
-
-    # prompt = template.format(input_content=input_content,style_content=style_content,number_words=number_words,social_media_type=media_type)
-
-    # client = OpenAI(
-    #     # This is the default and can be omitted
-    #     api_key=openai_api_key,
-    # )
-
-    # chat_completion = client.chat.completions.create(
-    #     messages=[
-    #         {
-    #             "role": "user",
-    #             "content": prompt,
-    #         }
-    #     ],
-    #     model="gpt-4o",
-    #     temperature=0.5
-    # )
-    
-
-
-
-    # submitted = st.form_submit_button("Submit")
-    # st.write("### Answer")
-    # output = chat_completion.choices[0].message.content
-    # st.info(output)
-
-    # st.write("### Number words")
-    # num_words_output = len(output.split())
-    # st.info(num_words_output)
+st.sidebar.header("Play Original Audio File")
+st.sidebar.audio(audio_file)
