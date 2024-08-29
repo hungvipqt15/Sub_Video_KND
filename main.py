245f06256e006eff066c393a7d356d1f810ca45d
hungvipqt15 <91128905+hungvipqt15@users.noreply.github.com>
2024-08-29 14:34:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from openai import OpenAI
 from prompt import transcribe_prompt
 import os
 from whisper.utils import get_writer
+from moviepy.editor import *
 
 st.title("Generate Subtitle App")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
-def video2mp3(video_file, output_ext="mp3"):
-    filename, ext = os.path.splitext(video_file)
-    subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{filename}.{output_ext}"],
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.STDOUT)
-    return f"{filename}.{output_ext}"
+# def video2mp3(video_file, output_ext="mp3"):
+#     filename, ext = os.path.splitext(video_file)
+#     subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{filename}.{output_ext}"],
+#                     stdout=subprocess.DEVNULL,
+#                     stderr=subprocess.STDOUT)
+#     return f"{filename}.{output_ext}"
+
+
+
+def MP4ToMP3(mp4, mp3):
+    FILETOCONVERT = AudioFileClip(mp4)
+    FILETOCONVERT.write_audiofile(mp3)
+    FILETOCONVERT.close()
+
 
 
 # upload audio file with streamlit
@@ -28,21 +37,22 @@ with st.form("my_form"):
 
 
         model = whisper.load_model("base")
-
+        audio_file_path = "audio.mp3"
 
 
         input_video = upload_file.name
+        print(input_video)
+        # audio_file = video2mp3(input_video)
+        MP4ToMP3(input_video, audio_file_path)
 
-        audio_file = video2mp3(input_video)
-
-        def translate(audio):
+        def translate(audio_file):
 
             options = dict(beam_size=5, best_of=5)
             translate_options = dict(task="transcribe", **options)
             result = model.transcribe(audio_file,**translate_options)
             return result
 
-        result = translate(audio_file)
+        result = translate(audio_file_path)
         client = OpenAI(api_key=openai_api_key)
         response = client.chat.completions.create(
         model="gpt-4o",
@@ -54,7 +64,7 @@ with st.form("my_form"):
         message = response.choices[0].message.content
         result = eval(message)
         output_dir = ''
-        audio_path = audio_file.split(".")[0]
+        audio_path = audio_file_path.split(".")[0]
         srt = os.path.join(output_dir, audio_path + ".srt")
         writer = get_writer("srt", output_dir)
         writer(result, srt)
